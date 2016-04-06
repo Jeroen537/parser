e8e359ffd5b64d94f784557cbb42d8639a384f73
Jeroen Bruijning <jeroen.bruijning@gmail.com>
2016-04-06 18:35:31 +0200

    irigrammar.txt ontwikkeld

---
+++ Parser/src/parsertools/parsers/iriparser.py
@@ -0,0 +1,19 @@
+'''
+Created on 28 mrt. 2016
+
+@author: jeroenbruijning
+'''
+from pyparsing import *
+from parsertools.extras import separatedList
+from parsertools.base import Parser
+from parsertools import ParsertoolsException
+
+#
+# Create the parser object
+#
+
+parser = Parser()
+
+#
+# Patterns
+#
